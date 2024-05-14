4a3a8d155e72e9ed883ba5ff793b7647ec446830
Ivan Roev <roevov2@gmail.com>
2024-05-14 18:46:16 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const audios = [
+    { 
+        number: [1830, 745],
+        text: "Vuonna 1830 seurassa oli jo 745 jäsentä...",
+        source: "Lyhyt historia lähes kaikesta, Bill Bryson"
+    },
+    { 
+        number: [1794],
+        text: "Vuonna 1794...",
+        source: "Lyhyt historia lähes kaikesta, Bill Bryson"
+    },
+    { 
+        number: [1785],
+        text: "Vuonna 1785...",
+        source: "Lyhyt historia lähes kaikesta, Bill Bryson"
+    },
+    { 
+        number: [1805],
+        text: "Parkinson hoiti museota vuoteen 1805...",
+        source: "Lyhyt historia lähes kaikesta, Bill Bryson"
+    },
+];
